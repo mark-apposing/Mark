77a601657743f4254df05ae01d80ceecdaf0187d
Mark Crane <robinjubber@unknown-00:24:36:eb:fa:40.home>
2013-04-23 10:29:00 +0100

    andy change

--- apposingIOSTest/AppDelegate.m
+++ apposingIOSTest/AppDelegate.m
@@ -14,6 +14,7 @@
 {
     // Override point for customization after application launch.
     return YES;
+    //andy change
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
